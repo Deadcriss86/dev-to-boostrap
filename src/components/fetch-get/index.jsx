ec5dc3d472ddb8573db983f3c0cd7d7971f392f3
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-02-27 15:54:53 -0600

    style and animations

--- src/components/fetch-get/index.jsx
+++ src/components/fetch-get/index.jsx
@@ -1,48 +1,68 @@
 import React, { useState, useEffect } from "react";
 import { CardPost } from "../card-post";
-import { Button } from "bootstrap";
-import CreaBoton from "../button/button";
-
+import './style.css';
+import { Link } from "react-router-dom";
 
 const FetchGet = (props) => {
-    const [cardData, setCardData] = useState([]);
-    const [search, setSearch] = useState("");
+  const [cardData, setCardData] = useState([]);
+  const [search, setSearch] = useState("");
 
-    const searcher = (e) => {
-        let letter = e.target.value
-        setSearch(letter);
-    }
+  const searcher = (e) => {
+    let letter = e.target.value;
+    setSearch(letter);
+  };
 
-    useEffect(() => {
-        const getAllData = async () => {
-            const response = await fetch('https://firstdatabase-c5db5-default-rtdb.firebaseio.com/post.json');
-            const data = await response.json();
-            const formattedData = Object.keys(data).map(key => ({ key, ...data[key] }));
-            setCardData(formattedData);
-            //refreshFilter();
-            
-        }
+  useEffect(() => {
+    const getAllData = async () => {
+      const response = await fetch(
+        "https://firstdatabase-c5db5-default-rtdb.firebaseio.com/post.json"
+      );
+      const data = await response.json();
+      const formattedData = Object.keys(data).map((key) => ({
+        key,
+        ...data[key],
+      }));
+      setCardData(formattedData);
+      //refreshFilter();
+    };
 
-        getAllData();
-    }, []);
+    getAllData();
+  }, []);
 
-    // Filtrar los datos cuando la búsqueda cambia
-    const results = !search ? cardData : cardData.filter((item) => item.Titulo.toLowerCase().includes(search.toLowerCase()));
+  // Filtrar los datos cuando la búsqueda cambia
+  const results = !search
+    ? cardData
+    : cardData.filter((item) =>
+        item.Titulo.toLowerCase().includes(search.toLowerCase())
+      );
 
+  return (
+    <>
+    <input 
+    value={search} 
+    onChange={searcher} 
+    type="text" 
+    placeholder='Search' 
+    className='form-control' 
+/>
 
-    return (
-        <>
-            <input value={search} onChange={searcher} type="text" placeholder='Search' className='form-control' />
-            <div className="">
-                {cardData && [...results].reverse().map(({ Titulo, Tags, UrlImagen, key }) => (
-                     <div key={key}>
-                     <CardPost UrlImagen={UrlImagen} Titulo={Titulo} Tags={Tags} />
-                     <CreaBoton path={`/details/${key}`} title="ver mas..." />
-                 </div>
-                ))}
+<div className="">
+    {cardData && [...results].reverse().map(({ Titulo, Tags, UrlImagen, key }) => (
+        <Link 
+            to={`/details/${key}`} 
+            key={key} 
+            className="text-dark d-block my-zoom-effect my-text-blue" // Añade las clases para el efecto de zoom y cambio de color
+            style={{ textDecoration: 'none' }} // Elimina la línea debajo del texto
+        >
+            <div className="text-dark">
+                <CardPost UrlImagen={UrlImagen} Titulo={Titulo} Tags={Tags} />
             </div>
-        </>
-    );
-}
+        </Link>
+    ))}
+</div>
+
+    </>
+  );
+};
 
 export default FetchGet;
