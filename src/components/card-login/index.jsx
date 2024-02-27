23b58729eddbaae50110e215f69c556e43e8d985
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-02-27 16:01:20 -0600

    changes style card login

--- src/components/card-login/index.jsx
+++ src/components/card-login/index.jsx
@@ -2,27 +2,25 @@ import { NavLink } from "react-router-dom";
 
 const CardLogin = () => {
   return (
-    <>
-      <div className="card m-auto fluid m">
-        <div className="card-body">
-          <h2 className="fs-5">
-            DEV Community is a community of 1,269,985 amazing developers
-          </h2>
-          <p className="card-text">
-            We're a place where coders share, stay up-to-date and grow their
-            careers.
-          </p>
-          <div class="d-grid gap-2 col-6 mx-auto">
-            <button class="btn btn-outline-primary" type="button">
-              Create Account
-            </button>
-            <button class="btn btn-outline-light" type="button">
-              <NavLink to="/Login">Log In</NavLink>
-            </button>
-          </div>
+    <div className="card m-auto fluid m">
+      <div className="card-body">
+        <h2 className="fs-5">
+          DEV Community is a community of 1,269,985 amazing developers
+        </h2>
+        <p className="card-text">
+          We're a place where coders share, stay up-to-date and grow their
+          careers.
+        </p>
+        <div className="d-grid gap-2 col-12 mx-auto">
+          <button className="btn btn-outline-primary btn-block" type="button">
+            Create Account
+          </button>
+          <NavLink to="/Login" className="btn btn-outline-light btn-block" type="button">
+            <span className="text-gray">Log In</span>
+          </NavLink>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
